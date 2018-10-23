7c59c1a8651d1b1f233233f7de72cc611d215fe4
daz1761 <dazgti@btinternet.com>
2018-10-23 19:50:30 +0100

    Finished API demo.

---
+++ DemoLibrary/SunProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SunProcessor
+    {
+        public static async Task<SunModel> LoadSunInformation(int comicNumber = 0)
+        {
+            string url = "https://api.sunrise-sunset.org/json?lat=41.494804&lng=-75.536852&date=today";
+
+            // this helps us dispose of things properly
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                // when finished, it will close everything down related to "response"
+                // because we dont want to leave ports open, and open up ports all the time
+
+                if (response.IsSuccessStatusCode)
+                {
+                    SunResultModel results = await response.Content.ReadAsAsync<SunResultModel>(); // ReadAsAsync uses the NewtonSoft Json converter which will try and map to our model
+                    return results.Results;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace WPFApp
         {
             InitializeComponent();
             ApiHelper.InitializeClient(); // start it in the root of our application
+            nextImageButton.IsEnabled = false;
         }
 
         private async Task LoadImage(int imageNumber = 0)
@@ -42,5 +43,43 @@ namespace WPFApp
         {
             await LoadImage();
         }
+
+        private async void previousImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentNumber > 1)
+            {
+                currentNumber -= 1;
+                nextImageButton.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if(currentNumber == 1)
+                {
+                    previousImageButton.IsEnabled = false;
+                }
+            }
+        }
+
+        private async void nextImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentNumber < maxNumber)
+            {
+                currentNumber += 1;
+                previousImageButton.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if(currentNumber == maxNumber)
+                {
+                    nextImageButton.IsEnabled = false;
+                }
+            }
+        }
+
+        private void sunInformationButton_Click(object sender, RoutedEventArgs e)
+        {
+            SunInfo sunInfo = new SunInfo();
+            sunInfo.Show();
+        }
+
+        // caching information does two things: it eases the pressure from a web server (minimum calls) - the rate of calls may be limited with an API.
     }
 }
